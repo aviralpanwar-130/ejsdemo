aa17051266ddb4a7fb55b732abba1c62b7e774d5
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-05-25 21:26:04 +0530

    first

---
+++ models/product.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  prize:{
+    type:Sequelize.INTEGER,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:Sequelize.STRING
+});
+
+module.exports = Product;
